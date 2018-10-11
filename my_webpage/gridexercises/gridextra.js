81268ef0c2776e2bfae4081b028e0b026edbc9f9
Aileen <pana005@newschool.edu>
2018-10-11 09:48:31 -0400

    Webpage

---
+++ my_webpage/gridexercises/gridextra.js
@@ -0,0 +1,22 @@
+for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i < 3) {
+            console.log("pink");
+            document.write("<div class='box pink'></div>");
+        }else if(i > 6){
+            console.log("yellow");
+            document.write("<div class='box yellow'></div>");
+        }else if(i = 3){
+            console.log("red");
+            document.write("<div class='box red'></div>");
+        }else {
+            console.log("turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
